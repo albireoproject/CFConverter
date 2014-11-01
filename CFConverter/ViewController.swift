5561ac0e7b05ee50c9068760cabe0a9fe5b6a7b4
Hongsun Yoon <albireo@Albireos-DevBook.local>
2014-11-01 23:25:04 +0900

    Initial Commit

--- CFConverter/ViewController.swift
+++ CFConverter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celciusValue: UITextField!
+    @IBOutlet weak var convertedFahrenheit: UILabel!
+    @IBOutlet weak var decoTextOne: UILabel!
+    @IBOutlet weak var fahrenheitValue: UITextField!
+    @IBOutlet weak var convertedCelcius: UILabel!
+    @IBOutlet weak var decoTextTwo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToFahrenheit(sender: UIButton) {
+
+        let celciusValueString = celciusValue.text
+        let celciusValueDouble = Double((celciusValueString as NSString).doubleValue)
+        let calculatedFahrenheit = (celciusValueDouble * 9) / 5 + 32
+        convertedFahrenheit.hidden = false
+        convertedFahrenheit.text = "\(calculatedFahrenheit)"
+        
+        decoTextOne.hidden = false
+        decoTextOne.text = "When " + "\(celciusValueString)" + " C is converted to F value..."
+        
+    }
+    
+    @IBAction func convertToCelcius(sender: UIButton) {
+        
+        let fahrenheitValueString = fahrenheitValue.text
+        let fahrenheitValueDouble = Double((fahrenheitValueString as NSString).doubleValue)
+        let calculatedCelcius = (fahrenheitValueDouble - 32) * 5 / 9
+        convertedCelcius.hidden = false
+        convertedCelcius.text = "\(calculatedCelcius)"
+        
+        decoTextTwo.hidden = false
+        decoTextTwo.text = "When " + "\(fahrenheitValueString)" + " F is converted to C value..."
+    }
+    
 
 }
 
